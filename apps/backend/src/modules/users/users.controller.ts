5462f1cf82b150bfaee1bc1542a94e0748ead296
alangripp <alangrippalencar@gmail.com>
2026-07-03 19:32:00 -0300

    Add Users controller

---
+++ apps/backend/src/modules/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    return this.usersService.getUserById(id);
+  }
+
+  @Put(':id/profile')
+  async updateProfile(@Param('id') id: string, @Body() data: any) {
+    return this.usersService.updateProfile(id, data);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
+  }
+}
